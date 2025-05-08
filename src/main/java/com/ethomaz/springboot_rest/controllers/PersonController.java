fa05eef962fd7612cfadc2361eacfc7ca6df1ac8
elcio-rosa <elcio.rosa@vodafone.com>
2025-05-08 11:04:03 +0100

    #TR# Content negotiation.

--- src/main/java/com/ethomaz/springboot_rest/controllers/PersonController.java
+++ src/main/java/com/ethomaz/springboot_rest/controllers/PersonController.java
@@ -25,34 +25,25 @@ public class PersonController {
     private PersonServices service;
     // private PersonServices service = new PersonServices();
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE })
     public List<PersonDTO> findAll() {
         return service.findAll();
     }
 
-    @GetMapping(value = "/{id}",
-        produces = MediaType.APPLICATION_JSON_VALUE
-    )
+    @GetMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE })
     public PersonDTO findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
 
-    @PostMapping(
-        consumes = MediaType.APPLICATION_JSON_VALUE,
-        produces = MediaType.APPLICATION_JSON_VALUE
-    )
+    @PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE })
     public PersonDTO create(@RequestBody PersonDTO person) {
         return service.create(person);
     }
 
-    @PutMapping(
-        consumes = MediaType.APPLICATION_JSON_VALUE,
-        produces = MediaType.APPLICATION_JSON_VALUE
-    )
+    @PutMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE })
     public PersonDTO update(@RequestBody PersonDTO person) {
         return service.update(person);
     }
-
 
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") Long id) {
--- src/main/java/com/ethomaz/springboot_rest/services/PersonServices.java
+++ src/main/java/com/ethomaz/springboot_rest/services/PersonServices.java
@@ -4,7 +4,6 @@ import static com.ethomaz.springboot_rest.mapper.ObjectMapper.parseListObjects;
 import static com.ethomaz.springboot_rest.mapper.ObjectMapper.parseObject;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,12 +18,10 @@ import com.ethomaz.springboot_rest.repository.PersonRepository;
 @Service
 public class PersonServices {
 
-    private final AtomicLong counter = new AtomicLong();
     private Logger logger = LoggerFactory.getLogger(PersonServices.class.getName());
 
     @Autowired
     PersonRepository repository;
-
 
     public List<PersonDTO> findAll() {
 
@@ -34,10 +31,11 @@ public class PersonServices {
     }
 
     public PersonDTO findById(Long id) {
-        logger.info("Finding one Person!");
 
+        logger.info("Finding one Person!");
         var entity = repository.findById(id)
-            .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
         return parseObject(entity, PersonDTO.class);
     }
 
@@ -54,7 +52,6 @@ public class PersonServices {
         logger.info("Updating one Person!");
         Person entity = repository.findById(person.getId())
                 .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
-
         entity.setFirstName(person.getFirstName());
         entity.setLastName(person.getLastName());
         entity.setAddress(person.getAddress());
@@ -66,7 +63,6 @@ public class PersonServices {
     public void delete(Long id) {
 
         logger.info("Deleting one Person!");
-
         Person entity = repository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
         repository.delete(entity);
